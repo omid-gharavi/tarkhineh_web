090bbcfb0daa5d448670a7bd015591cc47378765
OMID GHARAVI <omidbros3@gmail.com>
2025-06-15 19:32:16 +0330

    fix: gap between text and menu

--- src/components/shared/menu/menu.tsx
+++ src/components/shared/menu/menu.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Menu() {
     return (
         <div className="flex flex-col items-center">
-            <span className="text-2xl font-bold mt-12 mb-[153px]">منوی رستوران</span>
+            <span className="text-2xl font-bold mt-12 mb-[153px] max-laptop:mb-16">منوی رستوران</span>
             <div className="grid grid-cols-4 max-laptop:grid-cols-2 items-center justify-center gap-x-6 gap-y-28">
                 {
                     menu.map(({ title, image }, index) => (
