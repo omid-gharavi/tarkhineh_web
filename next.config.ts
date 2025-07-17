25e4b84c303f67afbc9bd5ab8a3073fa0518f08a
OMID GHARAVI <omidbros3@gmail.com>
2025-07-18 00:52:32 +0330

    add: sentry

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
+import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const sentryPluginOptions = withSentryConfig(
+  nextConfig,
+  {
+    silent: true,
+    widenClientFileUpload: true,
+    org: process.env.SENTRY_ORG,
+    project: process.env.SENTRY_PROJECT,
+  }
+);
+
+export default sentryPluginOptions;
--- src/components/shared/branches/branches.tsx
+++ src/components/shared/branches/branches.tsx
@@ -40,38 +40,36 @@ export default function Branches() {
     }
 
     return (
-        <>
-            <div className="pt-12">
-                <p className="font-bold text-2xl text-center">ترخینه گردی</p>
-                <div className="mt-6 grid grid-cols-[repeat(4,minmax(100px,280px))] max-laptop:grid-cols-1 justify-center gap-x-6 gap-y-3 px-5">
-                    {
-                        branchList.map(({ image, title, desc }, index) => (
+        <div className="pt-12">
+            <p className="font-bold text-2xl text-center">ترخینه گردی</p>
+            <div className="mt-6 grid grid-cols-[repeat(4,minmax(100px,280px))] max-laptop:grid-cols-1 justify-center gap-x-6 gap-y-3 px-5">
+                {
+                    branchList.map(({ image, title, desc }, index) => (
+                        <motion.div
+                            layout
+                            className="branch" key={index + 1}>
                             <motion.div
-                                layout
-                                className="branch" key={index + 1}>
-                                <motion.div
-                                    layoutId="cardContent"
-                                    className="branch-child"
-                                    onClick={() => toggleImage(index)}
-                                >
-                                    <Image
-                                        className="size-full object-cover overflow-hidden"
-                                        src={image}
-                                        alt="cover"
-                                        width={200}
-                                        height={200}
-                                    />
-                                </motion.div>
-                                <div>
-                                    <p className="font-semibold max-laptop:font-medium text-xl max-laptop:text-sm px-2 text-center max-laptop:text-right max-sm:text-center">{title}</p>
-                                    <p className="font-medium text-sm max-laptop:text-[10px] text-center px-2 mt-4 max-laptop:mt-2">{desc}</p>
-                                </div>
-                                <Button className="branch_btn max-laptop:hidden mt-2" variant='tintGlass'>صفحه شعبه</Button>
+                                layoutId="cardContent"
+                                className="branch-child"
+                                onClick={() => toggleImage(index)}
+                            >
+                                <Image
+                                    className="size-full object-cover overflow-hidden"
+                                    src={image}
+                                    alt="cover"
+                                    width={200}
+                                    height={200}
+                                />
                             </motion.div>
-                        ))
-                    }
-                </div>
+                            <div>
+                                <p className="font-semibold max-laptop:font-medium text-xl max-laptop:text-sm px-2 text-center max-laptop:text-right max-sm:text-center">{title}</p>
+                                <p className="font-medium text-sm max-laptop:text-[10px] text-center px-2 mt-4 max-laptop:mt-2">{desc}</p>
+                            </div>
+                            <Button className="branch_btn max-laptop:hidden mt-2" variant='tintGlass'>صفحه شعبه</Button>
+                        </motion.div>
+                    ))
+                }
             </div>
-        </>
+        </div>
     )
 }
